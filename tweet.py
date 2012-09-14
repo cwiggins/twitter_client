097dfd0ced47bc6f919f9a8b60835aaa0ca65a3f
Curtiss J Wiggins <cxly15@gmail.com>
2012-09-13 22:12:21 -0400

    My twitter oauth tokens have been removed.

--- tweet.py
+++ tweet.py
@@ -1,8 +1,14 @@
 class twitter_user(object):
-	def __init__(self, contributors_enabled = False,
-			created_at = None, default_profile = False, default_profile_image = False,
-			description = None, favorites_count = None, id_str = None,
-			in_reply_to_screen_name = None, in_reply_to_status_id_str = None,
-			in_reply_to_user_id_str = None, place = None, possibly_sensitive = False,
-			scopes = None, retweet_count = None, retweeted = False, source = None,
-			text = None, truncated = False):
+	def __init__(self, contributors_enabled = False, created_at = None,
+            default_profile_image = True, description = None, favorites_count = None,
+            followers_count = None, friends_count = None, geo_enabled = False,
+            id_str = None, is_translator = False, lang = None, listed_count = None,
+            location = None, name = None, profile_image_url = None,
+            profile_image_url_https = None, profile_use_background_image = True,
+            protected = False, screen_name = None, status = None,
+            statuses_count = None, time_zone = None, url = None, utc_offset = None,
+            verified = None, withheld_in_countries = None, withheld_scope = None):
+        self.contributors_enabled = contributors_enabled
+        self.created_at = created_at
+        self.default_profile_image = default_profile_image
+
--- twitter.py
+++ twitter.py
@@ -1,25 +1,20 @@
 from twython import Twython
 from format_timeline import *
-app_key = 'TkKK5AjoWzHSEdNejKiOQ'
-app_secret = 'zs7fk3KBSjdBQdU0Mks5mr80mdCuUivzOWODoW8RyE'
-oauth_token = '183630095-jvkb3yBqbbOVfM0liLYndJ7dOZbUnxnflKztIfyr'
-oauth_token_secret = 'n5HOGg76QBWslzr4zBaDE70rUfk2siHtxRYIQL6Eyc'
-t = Twython(app_key,
-		app_secret,
-		callback_url = 'http://google.com')
+import os
+oauth_tokens = open('oauth_tokens','r')
+app_key = oauth_tokens.readline()
+app_secret = oauth_tokens.readline()
+oauth_token = oauth_tokens.readline()
+oauth_token_secret = oauth_tokens.readline()
+
+t = Twython(app_key = app_key.strip('\n'), app_secret = app_secret.strip('\n'),
+        callback_url = 'http://google.com')
 
 auth_props = t.get_authentication_tokens()
-
-#oauth_token = auth_props['oauth_token']
-#oauth_token_secret = auth_props['oauth_token_secret']
-
 print 'Connect to Twitter via: %s' % auth_props['auth_url']
 
-t = Twython(app_key, app_secret, oauth_token = oauth_token,
-		oauth_token_secret = oauth_token_secret)
-
-auth_tokens = t.get_authorized_tokens()
-
+t = Twython(app_key = app_key.strip('\n'), app_secret = app_secret.strip('\n'),
+        oauth_token = oauth_token.strip('\n'), oauth_token_secret = oauth_token_secret.strip('\n'))
 homeTimeline = format_timeline(t.getHomeTimeline())
 
 #print homeTimeline
